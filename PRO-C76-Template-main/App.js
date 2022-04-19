34733bfd2ad9ce2c7e8399798a60bdec5f86c02a
yashtys <101933301+yashtys@users.noreply.github.com>
2022-04-19 18:15:31 +0530

    Update App.js

--- PRO-C76-Template-main/App.js
+++ PRO-C76-Template-main/App.js
@@ -22,7 +22,7 @@ function App() {
     
         {/* <Stack.Screen component="Home" name={HomeScreen} /> */}
         {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-        {/* <Stack.Screen name="Home" component="HomeScreen" /> */}
+        { <Stack.Screen name="Home" component="HomeScreen" /> }
         {/* <Stack.Screen name:"Home" component:{HomeScreen} /> */}
     
         <Stack.Screen name="StarMap" component={StartMapScreen} />
